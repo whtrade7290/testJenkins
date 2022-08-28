8578006939ca77cb415a6e5726af0f96a1517c54
WooHyeon <jeong_wh@stoneb.jp>
2022-08-28 22:49:58 +0900

    Initial commit

---
+++ src/main/java/com/example/springStartEx/SpringStartExApplication.java
@@ -0,0 +1,13 @@
+package com.example.springStartEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartExApplication.class, args);
+	}
+
+}
